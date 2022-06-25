882bcc3e50071b0ba18f34f5d8749dcae6f6bb57
arghya17 <arghyadeepsaha17@gmail.com>
2022-06-25 18:20:56 +0530

    preprocessed data added

--- ML_model/dataset.py
+++ ML_model/dataset.py
@@ -5,6 +5,7 @@ import os, csv
 from dotenv import load_dotenv
 import glob
 import pandas as pd
+import re
 load_dotenv()
 cid=os.getenv('cid')
 client_secret=os.getenv('client_secret')
@@ -73,4 +74,31 @@ def generate_dataset():
     df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
     df.to_csv('data/dataset.csv')
 
-generate_dataset()
+# generate_dataset()
+
+def ari_to_features(ari):
+
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=client_secret)
+    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+    
+    #Audio features
+    features = sp.audio_features(ari)[0]
+    
+    #Artist of the track, for genres and popularity
+    artist = sp.track(ari)["artists"][0]["id"]
+    artist_pop = sp.artist(artist)["popularity"]
+    artist_genres = sp.artist(artist)["genres"]
+    
+    #Track popularity
+    track_pop = sp.track(ari)["popularity"]
+    
+    #Add in extra features
+    features["artist_pop"] = artist_pop
+    if artist_genres:
+        features["genres"] = " ".join([re.sub(' ','_',i) for i in artist_genres])
+    else:
+        features["genres"] = "unknown"
+    features["track_pop"] = track_pop
+    
+    return features
