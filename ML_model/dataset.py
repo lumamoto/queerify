532e73e12a252551e9bc5e1c973a8b1f2819ed1f
arghya17 <arghyadeepsaha17@gmail.com>
2022-06-25 12:08:07 +0530

    spotify api added to make api calls

---
+++ ML_model/dataset.py
@@ -0,0 +1,42 @@
+from pydoc import cli
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import os
+from dotenv import load_dotenv
+load_dotenv()
+cid=os.getenv('cid')
+client_secret=os.getenv('client_secret')
+
+client_credentials_manager=SpotifyClientCredentials(client_id=cid,
+client_secret=client_secret)
+sp=spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+
+
+for track in sp.playlist_tracks(playlist_URI)["items"]:
+    #URI
+    track_uri = track["track"]["uri"]
+    
+    #Track name
+    track_name = track["track"]["name"]
+    
+    #Main Artist
+    artist_uri = track["track"]["artists"][0]["uri"]
+    artist_info = sp.artist(artist_uri)
+    
+    #Name, popularity, genre
+    artist_name = track["track"]["artists"][0]["name"]
+    artist_pop = artist_info["popularity"]
+    artist_genres = artist_info["genres"]
+    
+    #Album
+    album = track["track"]["album"]["name"]
+    
+    #Popularity of the track
+    track_pop = track["track"]["popularity"]
+
+print(track_uri)
+print(sp.audio_features(track_uri)[0])
