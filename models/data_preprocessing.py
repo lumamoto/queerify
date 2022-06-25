38550b34d98e9b422f6cbc46dceae535ef09da49
arghya17 <arghyadeepsaha17@gmail.com>
2022-06-26 00:28:52 +0530

    Readme updated and preprocessing further features added

--- models/data_preprocessing.py
+++ models/data_preprocessing.py
@@ -27,4 +27,31 @@ def data_preprocessing(dataset,savefile=None):
         new_df.to_csv(dataPath)
     else:
         new_df.to_csv(os.path.join('./data',savefile))
+    def drop_duplicate(df):
+        '''
+        Drop duplicate songs
+        '''
+        df['artists_song'] = df.apply(lambda row: row['artist_name']+row['track_name'],axis = 1)
+        return df.drop_duplicates('artists_song')
 
+    def select_columns(df):
+        return df[['artist_name','id','track_name','danceability','energy', 'key', 'loudness', 'mode',
+            'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', "artist_pop", "genres", "track_pop"]]
+    
+    def genre_preprocess(df):
+        '''
+        Preprocess the genre data
+        '''
+        df['genres_list'] = df['genres'].apply(lambda x: x.split(" "))
+        return df
+    def playlist_preprocesses(df):
+        df=drop_duplicate(df)
+        df=select_columns(df)
+        df=genre_preprocess(df)
+        return df
+    if savefile==None:
+        playlistDF_test=pd.read_csv(dataPath)
+    else:
+        playlistDF_test=pd.read_csv(os.path.join('./data',savefile))
+    playlistDF_test=playlist_preprocesses(playlistDF_test)
+    playlistDF_test.to_csv(os.path.join('./data',savefile))
